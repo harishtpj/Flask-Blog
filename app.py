bfaa02cce08e30614d628255a553b7fbcff7f067
harishtpj <harishtpj@gmail.com>
2021-06-23 07:38:16 +0000

    Added Features to View posts

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 import sqlite3
 from flask import Flask, render_template
+from werkzeug.exceptions import abort
 
 def connectdb():
     conn = sqlite3.connect('blog.db')
     conn.row_factory = sqlite3.Row
     return conn
+
+def getpost(id):
+    conn = connectdb()
+    post = conn.execute(f"Select * from posts where id = {id}").fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
 
 app = Flask(__name__)
 
@@ -14,6 +23,11 @@ def index():
     data = db.execute("Select * from posts").fetchall()
     db.close()
     return render_template("index.html", posts = data)
+
+@app.route("/<int:id>")
+def post(id):
+    data = getpost(id)
+    return render_template("post.html", post = data)
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port = 8080, debug = True)
