2dc6c6c3c4c0b3afec44f361ef1bd9d2178bc18e
harishtpj <harishtpj@gmail.com>
2021-06-23 07:25:59 +0000

    Added features to Display Blogs

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+import sqlite3
+from flask import Flask, render_template
+
+def connectdb():
+    conn = sqlite3.connect('blog.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1> Hello Flask </h1>"
+    db = connectdb()
+    data = db.execute("Select * from posts").fetchall()
+    db.close()
+    return render_template("index.html", posts = data)
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port = 8080, debug = True)
